8347e2656637c20c0129df604ae427e25354bfc2
DjonnyLem <djonnylem@gmail.com>
2022-02-15 20:15:43 +0400

    change dj_usart

--- DJ_USART/dj_usart.c
+++ DJ_USART/dj_usart.c
@@ -6,8 +6,10 @@ int main(void)
     while(1)
     {
 		
- 	 	USART_Transmit('O');//Передаем при включении
-  		USART_Transmit('k');//сообщение "Ok!", что свидетельствует
+ 	 	USART_Transmit('A');//Передаем при включении
+        USART_Transmit('N');//Передаем при включении
+        USART_Transmit('N');//Передаем при включении
+  		USART_Transmit('A');//сообщение "Ok!", что свидетельствует
   		USART_Transmit('!');//о правильно работе программы
   		USART_Transmit(0x0d);//переход в начало строки
   		USART_Transmit(0x0a);//переход на новую строку
