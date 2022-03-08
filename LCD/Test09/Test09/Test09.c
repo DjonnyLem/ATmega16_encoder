63af63e672431916fffeaa49d8d5930109528d58
DjonnyLem <djonnylem@gmail.com>
2022-03-08 20:21:34 +0400

    add LCD

---
+++ LCD/Test09/Test09/Test09.c
@@ -0,0 +1,20 @@
+#include "main.h"
+//----------------------------------------
+void port_ini(void)
+{
+	PORTD=0x00;
+	DDRD=0xFF;
+}
+//----------------------------------------
+int main(void)
+{
+	port_ini(); //Инициализируем порты
+	LCD_ini();  //Инициализируем дисплей
+	setpos(0,0);
+	str_lcd("Hello World!");
+	setpos(2,1);
+	str_lcd("String 2");
+	while(1)
+	{
+	}
+}
